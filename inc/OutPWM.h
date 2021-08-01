6a504dcf04b5e49d9d08e8a746700ef98da173d1
RAJIV ADAK <49756157+rajivadak@users.noreply.github.com>
2021-08-01 11:15:32 +0530

    Add files via upload

---
+++ inc/OutPWM.h
@@ -0,0 +1,26 @@
+/**
+ * @file OutPWM.h
+ * @author 260213 (you@domain.com)
+ * @brief Display the output of the temperature sensor
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __OUTPWM_H__
+#define __OUTPWM_H__
+#include<avr/io.h>
+/**
+ * @brief Function definition
+ * 
+ * @param temp_value 
+ * @return char
+ */
+char OutPWM(uint16_t temp_value);
+/**
+ * @brief port configuration for PWM
+ * 
+ */
+void InitPWM(void);
+#endif
---
+++ inc/UART.h
@@ -0,0 +1,33 @@
+/**
+ * @file UART.h
+ * @author 260213(you@domain.com)
+ * @brief UART serial communication
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __UART_H__
+#define __UART_H__
+#include<avr/io.h>
+/**
+ * @brief Inittialize UART serial communication ports
+ * 
+ * @param ubrr 
+ */
+void InitUART(uint16_t ubrr);
+/**
+ * @brief Read characters from UART port
+ * @return char
+ */
+char UARTread();
+/**
+ * @brief Write characters to UART port
+ * 
+ * @param data 
+ */
+void UARTwrite(char data);
+
+#endif
