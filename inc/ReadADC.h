59d922cad3a456dd8b87093a4ceb09c4cdc273fe
RAJIV ADAK <49756157+rajivadak@users.noreply.github.com>
2021-08-01 10:58:50 +0530

    Add files via upload

---
+++ inc/ReadADC.h
@@ -0,0 +1,22 @@
+/**
+ * @file ReadADC.h
+ * @author 260213 (you@domain.com)
+ * @brief ADC function definition
+ * @version 0.1
+ * @date 2021-04-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __READADC_H__
+#define __READADC_H__
+/**
+ * @brief Reading the analog input and returning a digital 8bit value
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+uint16_t ReadADC(uint8_t ch);
+void InitADC();
+#endif
---
+++ simulation/README.md
@@ -0,0 +1,4 @@
+## Activity 1
+![SS](https://github.com/260213/260213_EmbeddedC/blob/6147ad1808d7cda7377831cfc95e2dc4127601a4/simulation/Act1.PNG)
+## Activity 2
+![SS](https://github.com/260213/260213_EmbeddedC/blob/6147ad1808d7cda7377831cfc95e2dc4127601a4/simulation/Act2.PNG)
