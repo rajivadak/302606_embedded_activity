4682e6400c2fad2093f5d9a3ca5952d0499d2557
RAJIV ADAK <49756157+rajivadak@users.noreply.github.com>
2021-08-01 12:52:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 /**
  * @file main.c
- * @author 260213 (you@domain.com)
- * @brief Activity_1
+ * @author 302606 (you@domain.com)
+ * @brief Activity
  * @version 0.1
- * @date 2021-04-24
+ * @date 2021-08-01
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -12,6 +12,8 @@
 
 #include "ledstat.h"
 #include "ReadADC.h"
+#include "OutPWM.h"
+#include"UART.h"
 #include<avr/io.h>
 
 
@@ -23,19 +25,24 @@
 void peripheral_init(void)
 {
     /*Configure LED and Switch pins*/
-    DDRB|=(1<<PB1);
-    DDRD&=~(1<<PD0);
-    PORTD|=(1<<PD0);
-    PORTD|=(1<<PD1);
+    InitLED();
+    /*Configure ADC registers and pins*/
     InitADC();
+    /*Configure PWM registers and pins*/
+    InitPWM();
+    /*Configure UART serial communication pin*/
+    InitUART(103);
 }
-
-
+    
+   
+uint16_t temp;
+char temp_data;
 int main(void)
 {
-    uint16_t temp;
+    /*uint16_t temp;*/
     // Initialize peripherals
     peripheral_init();
+    
     while(1)
     {
         if(SENSOR_ON) //If switch_1 is ON
@@ -44,13 +51,21 @@ int main(void)
             {
                 ledstat(LED_ON);//LED is ON
                 temp=ReadADC(0);
+                temp_data = OutPWM(temp);
+                UARTwrite(temp_data);
+
+            }
+            else
+            {
+                
+                ledstat(LED_OFF);
             }
         }
         else
         {
             ledstat(LED_OFF);//LED is OFF
+            OCR1A=0;
         }
     }
     return 0;
 }
-
